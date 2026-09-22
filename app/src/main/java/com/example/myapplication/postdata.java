377931f22211ca41498dda8ed4dd48d3842175fd
Jiyou Hwang <fraguant5488@gmail.com>
2026-09-22 17:51:20 +0900

    feat: HTTP 통신용 comm_data 인터페이스, postdata 클래스, Retrofit 의존성 추가

---
+++ app/src/main/java/com/example/myapplication/postdata.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class postdata {
+
+    @Expose
+    @SerializedName("user")
+    private String user;
+
+    @Expose
+    @SerializedName("data")
+    private String data;
+
+    public void set_data(String user, String data) {
+        this.user = user;
+        this.data = data;
+    }
+
+    public void data_show() {
+        android.util.Log.e("test", user + data);
+    }
+
+    public String get_data() {
+        return data;
+    }
+}
