08b592db7b170b9238c54bd5d80006ddf45272da
C-sewon <cswley0130@naver.com>
2026-09-22 17:49:31 +0900

    create SensorResponse.java

---
+++ app/src/main/java/com/example/myapplication/SensorResponse.java
@@ -0,0 +1,13 @@
+public class SensorResponse {
+    private String result;
+    private String message;
+    private ReceivedData received_data;
+
+    public String getResult() { return result; }
+    public String getMessage() { return message; }
+
+    public static class ReceivedData {
+        private String team;
+        private String sensor;
+    }
+}
